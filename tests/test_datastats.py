12356a16056cd1cdb4422a82506ad3cdc559f976
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-15 15:41:49 +0100

    Step 3 - Refactoring the tests

--- tests/test_datastats.py
+++ tests/test_datastats.py
@@ -3,30 +3,32 @@ import json
 from datastats.datastats import DataStats
 
 
+test_data = [
+    {
+        "id": 1,
+        "name": "Laith",
+        "surname": "Simmons",
+        "age": 68,
+        "salary": "£27888"
+    },
+    {
+        "id": 2,
+        "name": "Mikayla",
+        "surname": "Henry",
+        "age": 49,
+        "salary": "£67137"
+    },
+    {
+        "id": 3,
+        "name": "Garth",
+        "surname": "Fields",
+        "age": 70,
+        "salary": "£70472"
+    }
+]
+
+
 def test_json():
-    test_data = [
-        {
-            "id": 1,
-            "name": "Laith",
-            "surname": "Simmons",
-            "age": 68,
-            "salary": "£27888"
-        },
-        {
-            "id": 2,
-            "name": "Mikayla",
-            "surname": "Henry",
-            "age": 49,
-            "salary": "£67137"
-        },
-        {
-            "id": 3,
-            "name": "Garth",
-            "surname": "Fields",
-            "age": 70,
-            "salary": "£70472"
-        }
-    ]
 
     ds = DataStats()
 
@@ -54,29 +56,6 @@ def test_json():
 
 
 def test__stats():
-    test_data = [
-        {
-            "id": 1,
-            "name": "Laith",
-            "surname": "Simmons",
-            "age": 68,
-            "salary": "£27888"
-        },
-        {
-            "id": 2,
-            "name": "Mikayla",
-            "surname": "Henry",
-            "age": 49,
-            "salary": "£67137"
-        },
-        {
-            "id": 3,
-            "name": "Garth",
-            "surname": "Fields",
-            "age": 70,
-            "salary": "£70472"
-        }
-    ]
 
     ds = DataStats()
 
