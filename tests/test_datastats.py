ab04add040d31820720030d8a3393dd05941900a
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-15 15:12:35 +0100

    Step 1 - Testing the endpoints

---
+++ tests/test_datastats.py
@@ -0,0 +1,53 @@
+import json
+
+from datastats.datastats import DataStats
+
+
+def test_json():
+    test_data = [
+        {
+            "id": 1,
+            "name": "Laith",
+            "surname": "Simmons",
+            "age": 68,
+            "salary": "£27888"
+        },
+        {
+            "id": 2,
+            "name": "Mikayla",
+            "surname": "Henry",
+            "age": 49,
+            "salary": "£67137"
+        },
+        {
+            "id": 3,
+            "name": "Garth",
+            "surname": "Fields",
+            "age": 70,
+            "salary": "£70472"
+        }
+    ]
+
+    ds = DataStats()
+
+    assert ds.stats(test_data, 20, 20000) == json.dumps(
+        {
+            'avg_age': 62,
+            'avg_salary': 55165,
+            'avg_yearly_increase': 837,
+            'max_salary': [{
+                "id": 3,
+                "name": "Garth",
+                "surname": "Fields",
+                "age": 70,
+                "salary": "£70472"
+            }],
+            'min_salary': [{
+                "id": 1,
+                "name": "Laith",
+                "surname": "Simmons",
+                "age": 68,
+                "salary": "£27888"
+            }]
+        }
+    )
