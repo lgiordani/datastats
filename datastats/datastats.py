658744fde0851876a9fd0443d8896f7920f7690a
Leonardo Giordani <giordani.leonardo@gmail.com>
2017-07-15 15:11:46 +0100

    Initial setup

---
+++ datastats/datastats.py
@@ -0,0 +1,38 @@
+import math
+import json
+
+
+class DataStats:
+
+    def stats(self, data, iage, isalary):
+        # iage and isalary are the starting age and salary used to
+        # compute the average yearly increase of salary.
+
+        # Compute average yearly increase
+        average_age_increase = math.floor(
+            sum([e['age'] for e in data])/len(data)) - iage
+        average_salary_increase = math.floor(
+            sum([int(e['salary'][1:]) for e in data])/len(data)) - isalary
+
+        yearly_avg_increase = math.floor(
+            average_salary_increase/average_age_increase)
+
+        # Compute max salary
+        salaries = [int(e['salary'][1:]) for e in data]
+        threshold = '£' + str(max(salaries))
+
+        max_salary = [e for e in data if e['salary'] == threshold]
+
+        # Compute min salary
+        salaries = [int(d['salary'][1:]) for d in data]
+        min_salary = [e for e in data if e['salary'] ==
+                      '£{}'.format(str(min(salaries)))]
+
+        return json.dumps({
+            'avg_age': math.floor(sum([e['age'] for e in data])/len(data)),
+            'avg_salary': math.floor(sum(
+                [int(e['salary'][1:]) for e in data])/len(data)),
+            'avg_yearly_increase': yearly_avg_increase,
+            'max_salary': max_salary,
+            'min_salary': min_salary
+        })
